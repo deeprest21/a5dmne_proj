1c78cfefd47875d0165820cc855f5a26b6bd6a97
omar <81667285+omar-121@users.noreply.github.com>
2023-04-10 16:29:24 +0300

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,1 +1,14 @@
-create table user(username varchar(255),password varchar(255),email varchar(255))
+create table user(
+  username varchar(255)
+  ,password varchar(255)
+  ,email varchar(255))
+  
+  
+create table services(
+  username varchar(255)
+  ,date varchar(255)
+  ,name_of_service varchar(255)
+path_of_the_pic varchar(255)
+)
+  
+  
