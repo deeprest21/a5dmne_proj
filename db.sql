1560348e6c7b9cda1d186acfc6c4a8f9bbe9cf78
omar <81667285+omar-121@users.noreply.github.com>
2023-04-10 21:48:04 +0300

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,14 +1,9 @@
-create table user(
-  username varchar(255)
+create table users(
+  username varchar(255) REFERENCES services(username)
   ,password varchar(255)
-  ,email varchar(255))
-  
-  
-create table services(
-  username varchar(255)
-  ,date varchar(255)
-  ,name_of_service varchar(255)
-path_of_the_pic varchar(255)
+  ,email varchar(255)
 )
+  
+ 
   
   
